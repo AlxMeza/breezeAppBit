def3b5c6b27a13d010d41043b764473ca42161e7
AlxMeza <alexis.meza24421@gmail.com>
2023-05-23 20:16:17 -0600

    add project to repository

---
+++ resources/js/Pages/Documents.jsx
@@ -0,0 +1,8 @@
+export default function Documents (){
+
+    return (
+        <>
+             <h1>Upload Files</h1>
+        </>
+    )
+}
---
+++ resources/js/app.jsx
@@ -0,0 +1,27 @@
+import './bootstrap';
+import '../css/app.css';
+
+import { createRoot } from 'react-dom/client';
+import { createInertiaApp } from '@inertiajs/react';
+import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
+
+const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+
+//theme
+import "primereact/resources/themes/lara-light-indigo/theme.css"     
+//core
+import "primereact/resources/primereact.min.css"
+import 'primeicons/primeicons.css'
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    setup({ el, App, props }) {
+        const root = createRoot(el);
+
+        root.render(<App {...props} />);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});
